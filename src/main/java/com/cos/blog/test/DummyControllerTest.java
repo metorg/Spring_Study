87d11c3bf0f6e0ef05cc9699400c76c6e829ac8b
metrog <metrog950@gmail.com>
2020-12-20 19:15:14 +0900

    User Delete 기능 구현

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -5,15 +5,20 @@ import java.util.List;
 import java.util.function.Supplier;
 
 import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -27,7 +32,43 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
 	
-	//http://localhost:8080/blog/dummy/users
+	// save함수는 id를 전달하지 않으면 insert를 해주고
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해요.
+	// email,password
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+		userRepository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당 아이디는 존재하지 않습니다.";
+		}
+		return "삭제되었습니다.id "+id;
+		
+	}
+	
+	@Transactional //함수 종료시에 자동 commit 이 됨
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {//RequestBody는 json 데이터를 받기위해사용
+																																		   //json 데ㅣ터를 요청 => Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아줘요.)
+		System.out.println("id : "+id);
+		System.out.println("password :"+requestUser.getPassword());
+		System.out.println("eamil :"+requestUser.getEmail());
+		
+		User user=userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+	
+		//userRepository.save(user);
+		
+		//더티 체킹
+		return user;
+	}
+	
+	//http://localhost:8000/blog/dummy/users
 	@GetMapping("/dummy/users")
 	public List<User> list(){
 		return userRepository.findAll();		
