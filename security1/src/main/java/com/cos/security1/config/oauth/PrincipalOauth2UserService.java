f5bfe07f82aefea63bd5c9444d4492eaea7e95a9
DESKTOP-BTO9KQS\leeseungmin <metrog950@gmail.com>
2021-05-25 18:51:08 +0900

    feat: 네이버 로그인 구현 완료

--- security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -2,6 +2,9 @@ package com.cos.security1.config.oauth;
 
 
 import com.cos.security1.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2Userinfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -28,16 +33,29 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest)throws OAuth2AuthenticationException{
         //System.out.println("userRequest:"+userRequest);
 
+        OAuth2User oauth2User = super.loadUser(userRequest);
         //구글로그인 버튼 클릭 -> 구글로그인창 -> 로그인을 완료 ->code를 리턴(OAuth-Client 라이브러리)->AcessToken요청
         //userRequest 정보 -> loadUser함수 호출 ->구글로부터 회원프로필 받아준다.
 
-        OAuth2User oauth2User = super.loadUser(userRequest);
 
-        String provider = userRequest.getClientRegistration().getClientId();//google
-        String providerId= oauth2User.getAttribute("sub");
+
+        //회원가입을 강제로 진행해볼 예정
+        OAuth2Userinfo oAuth2Userinfo =null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+            System.out.println("구글 로그인 요청");
+            oAuth2Userinfo= new GoogleUserInfo(oauth2User.getAttributes());
+
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            System.out.println("네이버 로그인 요청");
+            oAuth2Userinfo= new NaverUserInfo((Map)oauth2User.getAttributes().get("response"));
+        }
+        System.out.println(oAuth2Userinfo);
+
+        String provider = oAuth2Userinfo.getProvider();
+        String providerId= oAuth2Userinfo.getProviderId();
         String username=provider+"_"+providerId;//google_xxxx
         String password =bCryptPasswordEncoder.encode("승민");
-        String email= oauth2User.getAttribute("email");
+        String email= oAuth2Userinfo.getEmail();
         String role ="ROLE_USER";
 
         User userEntity = userRepository.findByUsername(username);
@@ -52,6 +70,8 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
                     .providerId(providerId)
                     .build();
             userRepository.save(userEntity);
+        }else{
+            System.out.println("로그인을 이미 한적이 있습니다. 당신은 자동회원가입이 되어 있습니다.");
         }
         return new PrincipalDetails(userEntity, oauth2User.getAttributes());
     }
